32270b4128f79193825bc5d287c4c4df34384dc8
FollowNaim <naimxf@gmail.com>
2025-01-23 01:04:00 +0600

    feat: created basic header for navigation
    Signed-off-by: FollowNaim <naimxf@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import Footer from "@/components/navigation/Footer";
+import Navbar from "@/components/navigation/Navbar";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Navbar />
+        <div className="min-h-screen">{children}</div>
+        <Footer />
       </body>
     </html>
   );
---
+++ components/navigation/Navbar.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+function Navbar() {
+  return (
+    <nav className="border-b">
+      <div className="container mx-auto flex justify-between items-center px-4 py-2">
+        <div>
+          <h2 className="text-2xl font-bold">Next Journal</h2>
+        </div>
+        <div>
+          <ul className="flex items-center gap-4">
+            <li>
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link href={"/about"}>About</Link>
+            </li>
+            <Link href={"/profile"}>Profile</Link>
+            <li>
+              <Button>Sign in</Button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
